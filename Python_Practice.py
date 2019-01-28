a3c9a31a1efcea547e841a2067d383283871ba0f
lanzhou65 <45521814+lanzhou65@users.noreply.github.com>
2019-01-28 16:52:02 -0500

    Create Python_Practice.py

---
+++ Python_Practice.py
@@ -0,0 +1,40 @@
+from collections import Counter
+import re
+from collections import defaultdict
+
+ii=['red', 'blue', 'red', 'green', 'blue', 'blue']
+cnt = Counter()
+for word in ['red', 'blue', 'red', 'green', 'blue', 'blue']:
+     cnt[word] += 1
+
+print(cnt)
+
+cnt1 = Counter()
+for i in [1,3,4,4,4,5]:
+  cnt1[i] +=1 
+
+print(cnt1)
+
+c = Counter([1,3,4,4,4,5])
+print('c is ', c)
+
+oo = Counter(['red', 'blue', 'red', 'green', 'blue', 'blue'])
+
+
+print('most common',oo.most_common(10))
+
+
+s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+d = defaultdict(list)
+for k , v in s:
+  d[k].append(v)
+
+print(d.items())
+
+yy = 'mississippi'
+y = defaultdict(int)
+for i in yy:
+  y[i] +=1 
+
+print(y)
+
